aebb6a1b5c8155db903d03a2cfcd6464df22f32e
Matthias Horne <infomh@anmaruco.de>
2015-11-05 23:06:11 +0100

    Add .dockerignore and docker-compose.yml.

--- run.sh
+++ run.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-export HOME=/home/openproject/
-echo $HOME && id
 source /home/openproject/.profile
 
 cd /ruby/openproject
 cp /var/config/configuration.yml config/configuration.yml
 cp /var/config/database.yml config/database.yml
-echo "copied config"
 
 # if Gemfile.plugins was changed
 bundle install
 npm install
-echo "installation complete"
 
 bundle exec rake db:create:all
 bundle exec rake generate_secret_token
