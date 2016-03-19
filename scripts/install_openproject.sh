90a4a47656892e22370cc96919d0a7a4d5f7a001
Gregor Hollmig <gregor@hollmig-online.de>
2016-03-19 21:28:21 +0100

    Use HTTP Only npm registry

--- scripts/install_openproject.sh
+++ scripts/install_openproject.sh
@@ -7,5 +7,6 @@ git clone https://github.com/opf/openproject-ce.git --branch stable/5 --depth 1 
 cd openproject
 git checkout v5.0.7
 gem install bundler
+npm config set registry http://registry.npmjs.org/
 bundle install --path vendor/bundle --without postgres sqlite development test therubyracer
 npm install
