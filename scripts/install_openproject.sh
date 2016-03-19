a15643b4ecaae770a4edcc101be30681aa6862e5
Gregor Hollmig <gregor@hollmig-online.de>
2016-03-19 21:28:58 +0100

    Switch OpenProject to 5.0.16

--- scripts/install_openproject.sh
+++ scripts/install_openproject.sh
@@ -5,7 +5,7 @@ cd /home/openproject
 source /home/openproject/.profile
 git clone https://github.com/opf/openproject-ce.git --branch stable/5 --depth 1 openproject
 cd openproject
-git checkout v5.0.7
+git checkout v5.0.16
 gem install bundler
 npm config set registry http://registry.npmjs.org/
 bundle install --path vendor/bundle --without postgres sqlite development test therubyracer
